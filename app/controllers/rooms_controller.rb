dd413b8c700f96f9d849549dc73755623cff0fe2
ALICHERIF <adem.mohamed@hotmail.fr>
2024-03-03 23:15:39 +0100

    rooms and bookings and users with authentification

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,5 @@
 class RoomsController < ApplicationController
-  before_action :set_room, only: [:show]
+  before_action :set_room, only: [:show, :edit, :update, :destroy]
   def index
     @rooms = Room.all
   end
@@ -9,7 +9,42 @@ class RoomsController < ApplicationController
   def new
 @room = Room.new
   end
-  def set_room
-@room = Room.find(params[:id])
+  def create
+    @room = Room.new(room_params)
+    respond_to do |format|
+      if @room.save
+    format.html {redirect_to room_url(@room), notice: "room was created succesfully"}
+    else
+    format.html {render :new, status: :unprocessable_entity}
+    end
   end
+  end
+  def edit
+
+  end
+def update
+  respond_to do |format|
+    if @room.update(room_params)
+      format.html { redirect_to @room, notice: 'Example was successfully updated.' }
+
+    else
+      format.html { render :edit, status: :unprocessable_entity }
+
+    end
+  end
+end
+def destroy
+   @room.destroy
+   respond_to do |format|
+     format.html { redirect_to root_path, notice: 'Example was successfully destroyed.' }
+   end
+ end
+  private
+  def set_room
+    @room = Room.find(params[:id])
+  end
+  def room_params
+    params.require(:room).permit(:name, :capacity)
+  end
+
 end
